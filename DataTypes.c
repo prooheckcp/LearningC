6c6c7e6bdc819aa3f1cf7fd4b39119ab6cd4a853
Prooheckcp <vasco.soares.2001@gmail.com>
2024-08-14 22:28:27 +0100

    Adding DataTypes

---
+++ DataTypes.c
@@ -0,0 +1,17 @@
+//
+// Created by Vasco Miguel Veenstra Soares on 14/08/2024.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+    int wholeNumber = 10;
+    float smallNumber = 33.333f;
+    double bigNumber = 5555.555555;
+    short shortNumber = 3;
+    bool myBool = true;
+    _Bool x = 0;
+
+    return 0;
+}
