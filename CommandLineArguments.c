77c9a8f3d01f9773c59a675299c21c3b4d003032
Prooheckcp <vasco.soares.2001@gmail.com>
2024-08-16 10:23:23 +0100

    Finishing C section 5

--- CommandLineArguments.c
+++ CommandLineArguments.c
@@ -1,3 +1,14 @@
 //
 // Created by Vasco Miguel Veenstra Soares on 15/08/2024.
 //
+
+#include <stdio.h>
+
+int main (int argc, char* argv[]){
+    char* argument1 = argv[0];
+    char* argument2 = argv[1];
+    8;
+    3-1;
+    printf("%s \n %s\ne", argument1, argument2);
+    return 0;
+}
