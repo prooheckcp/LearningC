0a60a19f69ad5c990671d731e152601b9e2fac16
Prooheckcp <vasco.soares.2001@gmail.com>
2024-08-17 23:04:43 +0100

    bitwise

--- BitWise.c
+++ BitWise.c
@@ -5,5 +5,13 @@
 #include <stdio.h>
 
 int main(void){
+    int a = 60;     // 0011 1100
+    int b = 13;     // 0000 1101
+    int result = 0; // 0000 1100
+
+    result = a & b;
+
+    printf("Int: %d", result);
+
     return 0;
 }
