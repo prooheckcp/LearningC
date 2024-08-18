d206c11d75371876c014d5779ed786b3f6b78b07
Prooheckcp <vasco.soares.2001@gmail.com>
2024-08-18 20:34:58 +0100

    OperatorOrder

--- OperatorOrder.c
+++ OperatorOrder.c
@@ -1,3 +1,16 @@
 //
 // Created by Vasco Miguel Veenstra Soares on 18/08/2024.
 //
+
+#include <stdio.h>
+
+int main(){
+    int i = 0;
+    int j = i++ + i;
+    printf("%d\n", j);
+
+    if (1 == 2 != 3){
+        printf("It was true!");
+    }
+    return 0;
+}
