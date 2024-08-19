a5f72b3f090b3ab4e87c2175c949eb239192e337
Prooheckcp <vasco.soares.2001@gmail.com>
2024-08-19 22:15:31 +0100

    Loops

---
+++ Loops.c
@@ -0,0 +1,12 @@
+//
+// Created by Vasco Miguel Veenstra Soares on 19/08/2024.
+//
+
+#include <stdio.h>
+
+int main(void){
+    for (int i = 1, j = 0; i <= 10; ++i)
+        printf("My integer: %d\n", i);
+
+    return 0;
+}
