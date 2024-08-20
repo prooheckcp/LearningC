6609646f07b226684a8bdef2db8cba32e2d56714
Prooheckcp <vasco.soares.2001@gmail.com>
2024-08-20 11:04:42 +0100

    Creating Sandbox

---
+++ Sandbox.c
@@ -0,0 +1,15 @@
+//
+// Created by Vasco Miguel Veenstra Soares on 19/08/2024.
+//
+
+#include <stdio.h>
+
+int main(void){
+    double k = 0;
+    for (k = 0.0; k < 3.0; k++);
+        printf("%lf", k);
+
+
+
+    return 0;
+}
