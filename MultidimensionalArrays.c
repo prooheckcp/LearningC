5a522c9e1394539dc57b5818138e358ec727b7cb
Prooheckcp <vasco.soares.2001@gmail.com>
2024-08-21 23:33:52 +0100

    Different types of arrays

--- MultidimensionalArrays.c
+++ MultidimensionalArrays.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main(void){
-    int matrix[10][10];
+    // 1 dimensional
+    int a[5] = {1, 2, 3};
+    // 2 dimensional
+    int matrix[10][10] = {
+            {1, 2, 3},
+            {4, 5, 6}
+    };
+    // 3 dimensional etc...
+    int b[5][10][15] = {
+            {{0, 1 ,2}},
+            {{0, 1, 2}},
+            {{1, 2, 3}},
+    };
 
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++)
